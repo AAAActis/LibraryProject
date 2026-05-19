8e6819bdb3bb6d9963571dfaf3088764490b6823
Santiago Joaquin Actis <santiactis6@gmail.com>
2026-05-18 23:38:58 -0300

    Struct: Creación clases base

---
+++ Libreria1/Clases/Libro.cs
@@ -0,0 +1,30 @@
+public class Libro
+{
+    private Guid _isbn;
+    private string _titulo;
+    private string _autor;
+    private bool _estaDisponible;
+
+    public Guid isbn => _isbn;
+    public string titulo => _titulo;
+    public string autor => _autor;
+    public bool estaDisponible => _estaDisponible;
+
+    public Libro(Guid isbn, string titulo, string autor)
+    {
+        _isbn = isbn;
+        _titulo = titulo;
+        _autor = autor;
+        _estaDisponible = true;
+    }
+
+    public void MarcarPrestado()
+    {
+        _estaDisponible = false;
+    }
+
+    public void MarcarDevuelto()
+    {
+        _estaDisponible = true;
+    }
+}
