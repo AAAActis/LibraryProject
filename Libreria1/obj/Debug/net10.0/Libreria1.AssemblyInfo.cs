fa05eb229ac3f7babd4e436ea97be8fc224c5b14
Santiago Joaquin Actis <santiactis6@gmail.com>
2026-05-19 21:28:24 -0300

    feat: implementación de IUsuarios.cs, corrección de la estructura del código, agregando encapsulación y verificando S de S.O.L.I.D

--- Libreria1/obj/Debug/net10.0/Libreria1.AssemblyInfo.cs
+++ Libreria1/obj/Debug/net10.0/Libreria1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Libreria1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d711b2c0c5ce26e17d6a2f90821a0ea041dc2291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f5e9303a2cf8420443030e5cf901565160bf617")]
 [assembly: System.Reflection.AssemblyProductAttribute("Libreria1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Libreria1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
