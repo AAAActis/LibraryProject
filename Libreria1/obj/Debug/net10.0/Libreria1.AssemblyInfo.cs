b243b0f9eec01f1a9aa9fe03ce777517dc5601ef
Santiago Joaquin Actis <santiactis6@gmail.com>
2026-05-27 21:55:22 -0300

    Creación de entidad repositorioPrestamos dentro de Program.

--- Libreria1/obj/Debug/net10.0/Libreria1.AssemblyInfo.cs
+++ Libreria1/obj/Debug/net10.0/Libreria1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Libreria1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38cb2815f9d72fcd1972ccd5368275e58dfde5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e58a46020b97b81134afa15f74b652d8a33dcd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Libreria1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Libreria1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
